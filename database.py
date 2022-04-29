bfa16badb8b0d8ccf186eeb83969a9ccc4fffccc
DanielRasho <ray22933@uvg.edu.gt>
2022-04-29 08:11:08 -0600

    some files added

---
+++ database.py
@@ -0,0 +1,17 @@
+
+def add_series():
+    pass
+
+def delete_series():
+    pass
+
+def update_series():
+    pass
+
+def get_series():
+    pass
+
+def show_menu():
+    print("Menú de opciones\n"
+          "1. Agregar serie\n"
+          "2. Modificar datos de alguna ser"  )
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # Primer commit
+
+from pickletools import OpcodeInfo
+from tkinter import Menu
+
+
+mostramos Menu
+usuario escoge opcion 
+
+si opcion == "agregar serie"
+    agregarserie()
+
+si opcion == "modificar datos de una seria"
+    ...
+
+si opcion == "ver lista de series"
+    ...
+
+si opcion == "imprimer el reporte"
+    ...
+
+si opcion == "salir"
+    exit()
